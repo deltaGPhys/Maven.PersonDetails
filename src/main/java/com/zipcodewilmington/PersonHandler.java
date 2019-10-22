eed1ecca7e2d5fb1fc60a7cb1a301d9158f93a8f
deltagphys <cty.jgates@gmail.com>
2019-10-22 15:21:57 -0400

    all tests passing

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -12,15 +12,12 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
-        // create a `counter`
-        // while `counter` is less than length of array
-            // begin loop
+        int i = 0;
+        while (i < this.personArray.length){
+            result += this.personArray[i].toString();
+            i++;
+        }
 
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-
-            // end loop
         return result;
     }
 
@@ -28,16 +25,9 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
-
-        // use the above clauses to declare for-loop signature
-            // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
+        for (int i = 0; i < this.personArray.length; i++){
+            result += this.personArray[i].toString();
+        }
 
         return result;
     }
@@ -46,14 +36,9 @@ public class PersonHandler {
 
     public String forEachLoop() {
         String result = "";
-        // identify array's type
-        // identify array's variable-name
-
-        // use the above discoveries to declare for-each-loop signature
-            // begin loop
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
+        for (Person p : this.personArray) {
+            result += p.toString();
+        }
 
         return result;
     }
